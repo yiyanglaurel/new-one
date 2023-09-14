93cf78de8cd54d5082adf60bc259de1787677490
yiyanglaurel <new one>
2023-09-14 10:09:43 +0800

    这是第五次作业

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
-git config --global user.name 'yangjh-xbmu'
-git config --global user.email 'yangjh@yeah.net'
+git config --global user.name 'yiyanglaurel'
+git config --global user.email 'new-one'
 git config --global http.sslverify false
 git add .
-git commit -m $1
+git commit -m ' 自动推送'
 git push
