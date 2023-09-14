1b1c0928307790a310dade13d7e3c2e60142c3d1
yiyanglaurel <1815204338@qq.com>
2023-09-14 21:00:10 +0800

    自动推送

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 git config --global user.name 'yiyanglaurel'
-git config --global user.email 'new-one'
+git config --global user.email '1815204338@qq.com'
 git config --global http.sslverify false
 git add .
 git commit -m '自动推送'
