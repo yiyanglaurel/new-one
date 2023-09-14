22c377907fa0c62c03177190dc188578a3f080e4
yiyanglaurel <new-one>
2023-09-14 20:14:36 +0800

    自动推送

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.name 'yiyanglaurel'
 git config --global user.email 'new-one'
 git config --global http.sslverify false
 git add .
-git commit -m ' 自动推送'
+git commit -m '自动推送'
 git push
